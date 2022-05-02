e29536816f4a0bcc8c1613225f9c88c1c4b94d35
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-05-02 18:21:37 +0500

    feat: создать схемы и модели

---
+++ models/card.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const ObjectId = mongoose.Schema.Types.ObjectId;
+
+// создаем схему
+const cardSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    minlength: 2,
+    maxlength: 30,
+    required: true,
+  },
+  link: {
+    type: String,
+    required: true,
+  },
+  owner: {
+    type: ObjectId,
+    required: true,
+  },
+  likes: [{
+    type: ObjectId,
+    default: [],
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  }
+});
+
+// создаём модель и экспортируем её
+module.exports = mongoose.model('card', cardSchema);
