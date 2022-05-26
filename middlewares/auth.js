2f73bd591b5e9b6b8a60a546abef810565ccf4e0
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-05-27 02:02:42 +0500

    feat: сздать мидлвэр для авторизации

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,6 +4,37 @@ const JWT_SECRET = 'themostclassifiedsecretsecret';
 
 const getToken = (id) => jwt.sign({ id }, JWT_SECRET, { expiresIn: '7d' });
 
+const isAuthtorized = async (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization || !authorization.startsWith('Bearer ')) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+
+  const token = authorization.replace('Bearer ', '');
+  let payload;
+
+  try {
+    payload = await jwt.verify(token, JWT_SECRET);
+  } catch (err) {
+    return res.status(401).send({ message: 'Необходима авторизация' });
+  }
+
+  req.user = payload;
+
+  next();
+};
+
 module.exports = {
   getToken,
+  isAuthtorized,
 };
+
+// const isAuthtorized = async (token) => {
+//   try {
+//     const decoded = await jwt.verify(token, JWT_SECRET);
+//     return !!decoded;
+//   } catch (err) {
+//     return false;
+//   }
+// };
