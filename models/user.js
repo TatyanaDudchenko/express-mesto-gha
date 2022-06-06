5d06ebca0706b0bde99c5068fec43362b88c9b76
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-07 01:07:21 +0500

    feat: доработать валидацию

--- models/user.js
+++ models/user.js
@@ -21,6 +21,7 @@ const userSchema = new mongoose.Schema({
     type: String,
     required: false,
     default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
+    validate: (v) => validator.isURL(v),
   },
   email: {
     type: String,
