35cb39bfd1b9f8d2dea4e89c8c2ccfd63dfc99ff
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-07 14:54:15 +0500

    feat: добавить защиту роутов неизвестного маршрута

--- routes/app.js
+++ routes/app.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { usersRoutes } = require('./users');
 const { cardsRoutes } = require('./cards');
 const NotFoundError = require('../errors/not-found-err');
+const { isAuthtorized } = require('../middlewares/auth');
 
 const routes = express.Router();
 
-routes.use('/users', usersRoutes);
-routes.use('/cards', cardsRoutes);
+routes.use('/users', isAuthtorized, usersRoutes);
+routes.use('/cards', isAuthtorized, cardsRoutes);
 routes.use((req, res, next) => {
   next(new NotFoundError('Маршрут не найден')); // 404
 });
