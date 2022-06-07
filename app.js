d788fffc5d3cc1149e017500c91221f25defc520
Tatyana Golendukhina <tanya.golenduhina@yandex.ru>
2022-06-07 16:40:15 +0500

    fix: удалить ненужный код

--- app.js
+++ app.js
@@ -15,11 +15,6 @@ app.post('/signup', express.json(), validationsCreateUser, createUser);
 
 app.use(routes);
 
-app.use((req, res, next) => {
-  console.log(req.method, req.url);
-  next();
-});
-
 app.use(errors()); // обработчик ошибок celebrate
 
 // здесь обрабатываем все ошибки
